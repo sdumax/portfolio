60157ac6dc354b3ea22eadd70e7e92de034ac675
Max <maxdiogu@gmail.com>
2020-08-25 08:09:21 +0100

    portfolio version1.2

---
+++ assets/js/nav.js
@@ -0,0 +1,9 @@
+function openMenu() {
+  document.getElementById("side-menu").style.width = "100%";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeMenu() {
+  document.getElementById("side-menu").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
